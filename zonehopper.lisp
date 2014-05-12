7c71ea7ead3c27f9d51626b2880f87b0ffc6b518
Futuro <evan@cs.umn.edu>
2014-05-11 22:28:47 -0500

    Added a named prepared search
    I added 'transit-within' to make it easier to search for transit types
    around an origin, since I'll need this for aggregating nodes.

--- zonehopper.lisp
+++ zonehopper.lisp
@@ -38,3 +38,26 @@
   (let ((result '()))
     (dolist (tag alist result)
       (push (cdr tag) result))))
+
+;; 'distance' could be meters, or it could be whatever unit the geography
+;; is in. Currently I don't care.
+(pomo:defprepared-with-names transit-within (origin_id distance type)
+	((:with
+	  (:as 'origin
+	       (:select '*
+			:from 'public.planet_osm_point
+			:where (:= 'osm_id '$1)))
+	  (:select 'point.osm_id 'point.highway 'point.name
+		   'point.operator 'point.way 'nodes.tags
+		   :from (:as 'public.planet_osm_point 'point)
+		   :inner-join 'origin
+		   :on (:st_dwithin 'origin.way 'point.way '$2)
+		   :inner-join (:as 'public.planet_osm_nodes 'nodes)
+		   :on (:= 'point.osm_id 'nodes.id)
+		   :where (:= 'point.highway '$3))) origin_id distance type)
+	:alists)
+
+#+NIL (transit-within 622228699 1000.0 "bus_stop")
+; Bus stops 1000 units around Coffman Union
+#+NIL (transit-within 600583817 1000.0 "bus_stop")
+; Bus stops 1000 units around Patina on Selby & Snelling
