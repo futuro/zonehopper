bc7b7eec66bded24a314a910f071266103524d86
Futuro <evan@cs.umn.edu>
2014-05-11 20:52:41 -0500

    Removed old code
    I removed the code I'd written to calculate distances between latitude
    and longitude because postgis will do all that for me.

--- zonehopper.lisp
+++ zonehopper.lisp
@@ -2,40 +2,6 @@
 
 (in-package #:zonehopper)
 
-; Turns out postgis can do all this biz for me! Time to play around with that
-(defparameter *earth-radius-miles* 3959 "Radius of the earth in miles")
-(defparameter *earth-radius-km* 6371 "Radius of the earth in kilometers")
-(defparameter *rad-converter* (/ pi 180) "Constant part of radian conversion")
-(defparameter *deg-converter* (/ 180 pi) "Constant part of degree conversion")
-
-(defun deg->rad (degrees)
-  (* degrees *rad-converter*))
-
-(defun rad->deg (rads)
-  (* rads *deg-converter*))
-
-(defun haversine (theta)
-  (expt (sin (/ theta 2)) 2))
-
-(defun distance-rads (lat1 lon1 lat2 lon2)
-  (let* ((h-lat (haversine (- lat2 lat1)))
-	 (h-lon (haversine (- lon2 lon1)))
-	 (alpha (+ h-lat (* h-lon (cos lat1) (cos lat2)))))
-    (* 2 *earth-radius-km* (atan (sqrt alpha) (sqrt (- 1 alpha))))))
-
-(defun distance-degs (lat1 lon1 lat2 lon2)
-  (distance-rads (deg->rad lat1)
-		 (deg->rad lon1)
-		 (deg->rad lat2)
-		 (deg->rad lon2)))
-
-(defun distance-list (coord-list)
-  (let ((lat1 (first (first coord-list)))
-	(lon1 (second (first coord-list)))
-	(lat2 (first (second coord-list)))
-	(lon2 (second (second coord-list))))
-    (distance-degs lat1 lon1 lat2 lon2)))
-
 (defun newstrlist->plist (strlist)
   "Create a new plist and convert a list of strings into it"
   (progn
